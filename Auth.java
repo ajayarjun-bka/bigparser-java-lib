f7fcb098d4c7578b72c58ecfce00501db0c54175
Ajay Arjun <arjun.bka@gmail.com>
2017-08-14 10:29:54 -0400

    restart

--- Auth.java
+++
@@ -1,9 +0,0 @@
-package base;
-import base.BigParser;
-public class Auth {
-	public static String login(String emailId, String password)
-	{
-		String authId = BigParser.login(emailId, password);
-		return authId;
-	}
-}
